8e17c6f78db5dcf23cd6b634186fff24e9d162f6
cowane-alt <cowane@staff.lisd.net>
2026-02-27 15:17:05 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Optional: Apps Script endpoint to email/save (see Code.gs below).
-// Leave empty to disable emailing.
+// config.js
 window.PHOTOBOOTH_CONFIG = {
   GAS_POST_URL: "https://script.google.com/macros/s/AKfycbzUVMniRgk9CdqPpF4x223YSiikm2Clmi8OYfgsNlW8OVmNKQ0LzDHYHLC5NDoAtiCI/exec"
 };
