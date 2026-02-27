6eaa3b753d7aae82f5f5e922ca0e84e6b0cbfc2d
cowane-alt <cowane@staff.lisd.net>
2026-02-27 14:48:59 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Optional: Apps Script endpoint to email/save (see Code.gs below).
+// Leave empty to disable emailing.
+window.PHOTOBOOTH_CONFIG = {
+  GAS_POST_URL: "" // e.g. "https://script.google.com/macros/s/XXXXXXXXXXXX/exec"
+};
